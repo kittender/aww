612ebee80dacee1c238d50292304f2c976cee3a1
Kittender =(._. )= <kittender@vivaldi.net>
2018-01-26 19:48:22 +0100

    tech(build): added suffixing and minification

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const myth = require('gulp-myth');
 const crass = require('gulp-crass');
 const concat = require('gulp-concat');
+const clean = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
 const root = './src/';
 const stack = [
@@ -11,6 +13,7 @@ const stack = [
 ];
 
 gulp.task("build", function () {
+    
     return gulp.src(stack)
         .pipe(concat('aww.css'))
         .pipe(myth())
@@ -18,6 +21,18 @@ gulp.task("build", function () {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task("default", ["build"], function () {
-    gulp.watch(stack, ["build"]);
+gulp.task("minify", ["build"], function () {
+    
+    return gulp.src('dist/aww.css')
+        .pipe(clean({debug: true}, (details) => {
+          console.log(`aww.css: ${details.stats.originalSize}`);
+          console.log(`aww-min.css: ${details.stats.minifiedSize}`);
+        }))
+        .pipe(rename({suffix: "-min", extname: ".css"}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task("default", ["minify"], function () {
+    
+    gulp.watch(stack, ["minify"]);
 });
